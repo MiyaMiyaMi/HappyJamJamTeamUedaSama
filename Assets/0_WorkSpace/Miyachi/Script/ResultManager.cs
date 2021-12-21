483e3e3a68a49488d138a3a4f8b5017c919aef73
MiyaMiyaMi <miyachi.kaito02@gmail.com>
2021-12-21 14:52:16 +0900

    OKOK

---
+++ Assets/0_WorkSpace/Miyachi/Script/ResultManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour
+{
+
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            var time = 123.4f;
+        
+            naichilab.RankingLoader.Instance.SendScoreAndShowRanking(time, 0);
+        }
+    }
+}
