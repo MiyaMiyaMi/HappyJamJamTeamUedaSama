47e670504918d7500280a92f55e12360d407cbfc
MiyaMiyaMi <miyachi.kaito02@gmail.com>
2021-12-21 15:02:16 +0900

    a

---
+++ Assets/0_WorkSpace/Miyachi/Script/BackScrolling.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BackScrolling : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed; //背景をスクロールさせるスピード
+    [SerializeField] private float startLine;//背景のスクロールを開始する位置
+    [SerializeField] private float deadLine; //背景のスクロールが終了する位置
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(scrollSpeed * Time.deltaTime, 0, 0); //x座標をscrollSpeed分動かす
+
+        if (transform.position.x < deadLine) //もし背景のx座標よりdeadLineが大きくなったら
+        {
+            transform.position = new Vector3(startLine, 0, 0);//背景をstartLineまで戻す
+        }
+
+    }
+ 
+}
