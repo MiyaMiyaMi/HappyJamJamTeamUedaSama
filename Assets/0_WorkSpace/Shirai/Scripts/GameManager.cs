7d1ad899f7169751862e88802ae2934fd5f758a6
MiyaMiyaMi <miyachi.kaito02@gmail.com>
2021-12-23 16:36:06 +0900

    d

--- Assets/0_WorkSpace/Shirai/Scripts/GameManager.cs
+++ Assets/0_WorkSpace/Shirai/Scripts/GameManager.cs
@@ -69,7 +69,7 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
                     SpawnValue = SpawnTime;           // 1秒にする
 
                     Number = Random.Range(0, CapsulePrefabs.Length); // Random.Range(最小値、最大値)整数の場合の最大値は除外
-                    Instantiate(CapsulePrefabs[Number], new Vector3(10, 0, 0), Quaternion.identity);    // X座標10からランダム生成
+                    Instantiate(CapsulePrefabs[Number], new Vector3(12, 0, 0), Quaternion.identity);    // X座標10からランダム生成
                 }
                 if (GameSpeed < maxGameSpeed)
                 {
